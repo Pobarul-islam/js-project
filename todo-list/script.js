7860595ae95099b1161edf4e5c46e38cf67c3cd5
Pobarul-islam <pobarul3@gmail.com>
2023-09-15 16:49:25 +0600

    edit updated

--- todo-list/script.js
+++ todo-list/script.js
@@ -13,14 +13,16 @@ document.getElementById('add-btn').addEventListener('click', function (e) {
               <td>${count}</td>
               <td>${inputValue}</td>
               <td>
-              <button class="delete-btn">Delete</button>
-              <button class="done-btn">Done</button>
+                <button class="edit-btn">Edit</button>
+                <button class="delete-btn">Delete</button>
+                <button class="done-btn">Done</button>
               </td>`;
     mainContainer.appendChild(tableContainer);
     document.getElementById('input-value').value = '';
 
     const deleteBtn = document.getElementsByClassName('delete-btn');
     const doneBtn = document.getElementsByClassName('done-btn');
+    const editBtn = document.getElementsByClassName('edit-btn');
 
     for (const button of deleteBtn) {
       button.addEventListener('click', function (e) {
@@ -33,11 +35,20 @@ document.getElementById('add-btn').addEventListener('click', function (e) {
         e.target.parentNode.parentNode.style.textDecoration = 'line-through';
       });
     }
+
+    for (const button of editBtn) {
+      button.addEventListener('click', function (e) {
+        const taskText = e.target.parentNode.parentNode.querySelector('td:nth-child(2)');
+        const newTask = prompt('Edit Task:', taskText.textContent);
+        if (newTask !== null) {
+          taskText.textContent = newTask;
+        }
+      });
+    }
   }
 });
 
-
 const clearAll = document.querySelector('#clear-all');
 clearAll.addEventListener('click', function (e) {
-  document.getElementById("table-content").parentNode.style.display="none";
-})
+  document.getElementById("table-content").parentNode.style.display = "none";
+});
